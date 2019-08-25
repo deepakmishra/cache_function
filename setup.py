76be307fb5703b85329802229baa90d7f9425db4
deepak mishra <deepak.mishra@hackerearth.com>
2019-08-26 03:30:21 +0530

    version change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cache-function-deepakmishra",
-    version="0.0.1",
+    version="0.0.2",
     author="Deepak Mishra",
     author_email="deepakmishra117@gmail.com",
     description="A small decorator which helps cache a result of a function",
